f86ba3fa349d3bc88f174c5d0586c4ec06f83c41
ultraman <1394466835@qq.com>
2024-08-09 15:44:48 +0800

    feat: a simple red sphere

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,21 @@ use tracer::ray::*;
 use tracer::utils::*;
 use tracer::vec3::*;
 
+fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = center - r.orig;
+    let a = r.dir.dot(r.dir);
+    let b = r.dir.dot(oc) * -2.0;
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Vec3 {
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.dir.unit();
     let a = 0.5 * (unit_direction.y + 1.0);
     Vec3::ones() * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a
